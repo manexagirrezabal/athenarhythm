5ec1f9d45371b582114983d128f50f787b56e442
Manex <manex.agirrezabal@gmail.com>
2016-03-11 17:20:04 +0100

    Changed the call to phonetisaurus, compatible with the last version.

--- LING/libraries/G2P.pm
+++ LING/libraries/G2P.pm
@@ -1,7 +1,7 @@
 package G2P;
 my $model = '/Users/manex/phonetisaurus/phonetisaurus-0.7.5/script/toy/new.fst'; #The model that we will use to do the G2P conversion
-my $model = '/sc01a4/users/maguirrezaba008/athenarhythm/toy.fst';
-my $phonetisaurusDir=''; #Phonetisaurus program location. Empty if it is in the $PATH variable
+my $model = '/home/magirrezaba008/athenarhythm-working/athenarhythm/LING/newdicg2p.fst';
+my $phonetisaurusDir='/home/magirrezaba008/Phonetisaurus/src/bin/'; #Phonetisaurus program location. Empty if it is in the $PATH variable
 
 
 
@@ -15,13 +15,13 @@ foreach my $word (@input) {
 }
 close(TMPFILE);
 
-my $res = `${phonetisaurusDir}phonetisaurus-g2p --model=$model --isfile --input=tmp.txt`;
+my $res = `${phonetisaurusDir}phonetisaurus-g2pfst --model=$model --wordlist=tmp.txt`;
 
 chomp($res);
 my @res = split(/\n/, $res);
 #print "g2p: ".$res."\n";
 foreach my $el (@res) {
-  $el =~ s/[^\t]*\t//;
+  $el =~ s/[^\t]*\t[^\t]*\t//;
   $el =~ s/\ //g;
 }
 return @res;
